2d3e4ce96e5a117362a1b42d2ea8b5476e70f398
frankpagan <frank@cocreate.app>
2024-04-01 16:20:14 -0500

    fix: updated config to support ffmpeg dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@
 
 const path = require("path")
 
-// const TerserPlugin = require("terser-webpack-plugin")
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-// const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = (env, argv) => {
     let isProduction = false
@@ -18,80 +18,84 @@ module.exports = (env, argv) => {
         output: {
             path: path.resolve(__dirname, "dist"),
             filename: isProduction ? "[name].min.js" : "[name].js",
-            // libraryTarget: "umd",
-            // libraryExport: "default",
-            // library: ["CoCreate", "ffmpeg"],
-            // globalObject: "this",
+            libraryTarget: "umd",
+            libraryExport: "default",
+            library: ["CoCreate", "ffmpeg"],
+            globalObject: "this",
         },
         experiments: {
             asyncWebAssembly: true,
             topLevelAwait: true,
         },
 
-        // plugins: [
-        //     new CleanWebpackPlugin(),
-        //     new MiniCssExtractPlugin({
-        //         filename: "[name].css",
-        //     }),
-        // ],
-        // Default mode for Webpack is production.
-        // Depending on mode Webpack will apply different things
-        // on final bundle. For now we don't need production's JavaScript
-        // minifying and other thing so let's set mode to development
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+            }),
+        ],
+
         mode: isProduction ? "production" : "development",
-        // module: {
-        //     rules: [
-        //         {
-        //             test: /.js$/,
-        //             exclude: /(node_modules)/,
-        //             use: {
-        //                 loader: "babel-loader",
-        //                 options: {
-        //                     plugins: ["@babel/plugin-transform-modules-commonjs"],
-        //                 },
-        //             },
-        //         },
-        //         {
-        //             test: /.css$/i,
-        //             use: [
-        //                 { loader: "style-loader", options: { injectType: "linkTag" } },
-        //                 "file-loader",
-        //             ],
-        //         },
-        //     ],
-        // },
+        module: {
+            rules: [
+                {
+                    test: /.js$/,
+                    exclude: (modulePath) => {
+                        // Additionally, exclude `CoCreate-ffmpeg.js` file
+                        if (/ffmpeg/.test(modulePath)) {
+                            return true;
+                        }
+                        // Include all other .js files
+                        return false;
+                    },
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            plugins: ["@babel/plugin-transform-modules-commonjs"],
+                        },
+                    },
+                },
+                {
+                    test: /.css$/i,
+                    use: [
+                        { loader: "style-loader", options: { injectType: "linkTag" } },
+                        "file-loader",
+                    ],
+                },
+            ],
+        },
 
         // add source map
         ...(isProduction ? {} : { devtool: "eval-source-map" }),
 
-        // optimization: {
-        //     minimize: true,
-        //     minimizer: [
-        //         new TerserPlugin({
-        //             extractComments: true,
-        //             // cache: true,
-        //             parallel: true,
-        //             // sourceMap: true, // Must be set to true if using source-maps in production
-        //             terserOptions: {
-        //                 // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-        //                 // extractComments: 'all',
-        //                 compress: {
-        //                     drop_console: true,
-        //                 },
-        //             },
-        //         }),
-        //     ],
-        //     splitChunks: {
-        //         chunks: "all",
-        //         minSize: 200,
-        //         // maxSize: 99999,
-        //         //minChunks: 1,
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    extractComments: true,
+                    // cache: true,
+                    parallel: true,
+                    // sourceMap: true, // Must be set to true if using source-maps in production
+                    terserOptions: {
+                        // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+                        // extractComments: 'all',
+                        compress: {
+                            drop_console: true,
+                        },
+                    },
+                }),
+            ],
+            // splitChunks: {
+            //     chunks: "all",
+            //     minSize: 200,
+            //     // maxSize: 99999,
+            //     //minChunks: 1,
 
-        //         cacheGroups: {
-        //             defaultVendors: false,
-        //         },
-        //     },
-        // },
+            //     cacheGroups: {
+            //         defaultVendors: false,
+            //     },
+            // },
+        },
     }
     return config
 }
